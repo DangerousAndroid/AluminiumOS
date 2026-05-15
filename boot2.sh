4e0ef0d469eb1c253cc5745759d6d88908ab214c
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-05-15 13:16:00 +0200

    boot2: Update backup

--- boot2.sh
+++ boot2.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
-# Configuration
-GSI="alos.img"
+GSI="super_alos.img"
 KERNEL="./p9pf/boot/kernel"
-RAMDISK="./p9pf/init_boot/ramdisk.cpio.img"
+RAMDISK="./p9pf/vendor_boot/alos.cpio"
+DTB="./dtb/alos.dtb"
 CORES="12"
 MEM="16G"
 
 echo "Launching AluminiumOS (Build CP1A.260305.018)..."
-
-# Note: earlycon and console are moved to the front for logging stability
 qemu-system-aarch64 \
   -M virt,gic-version=3 \
   -cpu max \
@@ -18,8 +15,9 @@ qemu-system-aarch64 \
   -accel tcg,thread=multi \
   -kernel "$KERNEL" \
   -initrd "$RAMDISK" \
+  -dtb "$DTB" \
   -drive file="$GSI",if=virtio,format=raw,readonly=on \
-  -append "earlycon=pl011,0x09000000 console=ttyAMA0 root=/dev/vda rw init=/init androidboot.hardware=virtio androidboot.boot_devices=pci0000:00/0000:00:05.0 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=vda androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange androidboot.fstab_suffix=virtio" \
+  -append "earlycon=pl011,0x09000000 console=ttyAMA0 root=/dev/vda rw init=/init androidboot.hardware=zumapro androidboot.boot_devices=4010000000.pcie androidboot.boot_devices=pci0000:00/0000:00:05.0 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=vda androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange" \
   -device virtio-gpu-gl-pci \
   -display gtk,gl=on \
   -device virtio-tablet-pci \
