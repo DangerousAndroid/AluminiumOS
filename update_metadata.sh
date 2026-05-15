2299b170406ac88d159e5c909731097d762c19d2
BasGame1 <abadbasantaalex@gmail.com>
2026-05-15 14:12:42 +0200

    update metadata: Init helper script

---
+++ update_metadata.sh
@@ -0,0 +1,10 @@
+sudo losetup -Pf super_disk.img
+LOOP_DEV=$(losetup -a | grep "super_disk.img" | awk -F: '{print $1}' | head -n 1)
+if [ -z "$LOOP_DEV" ]; then
+    echo "Error: Could not map super_disk.img to a loop device."
+    exit 1
+fi
+sudo dd if=metadata.img of="${LOOP_DEV}p6" bs=4M status=progress
+sync
+sudo losetup -d "$LOOP_DEV"
+echo "Super updated in disk!"
