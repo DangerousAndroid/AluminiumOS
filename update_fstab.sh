8c0286052485b40531623996f146c600db40215b
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-05-17 14:59:10 +0200

    update metadata, update fstab: Update helper scripts

--- update_fstab.sh
+++ update_fstab.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+ROOT_DIR=$(pwd)
+cd p9pf/vendor_boot
 magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zuma-fips fstab.zuma-fips" && magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zuma fstab.zuma" && magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zumapro-fips fstab.zumapro-fips" && magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zumapro fstab.zumapro" && magiskboot cpio alos.cpio "add 0750 system/etc/recovery.fstab recovery.fstab"
+cd $ROOT_DIR
