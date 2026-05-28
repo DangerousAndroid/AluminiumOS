ca6d2fc82a1f1aee335cffc45388a967e4c66e03
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-05-28 14:05:40 +0200

    move keymint: Init script to use keymint when needed

---
+++ move_keymint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ ! -f keymint/android.hardware.gatekeeper-service.trusty ] || [ ! -f keymint/android.hardware.keymaster@4.0-service.rc ] || [ ! -f keymint/android.hardware.gatekeeper-service.trusty ]; then
+ echo "Files missing!"
+ exit 255
+fi
+for i in android.hardware.gatekeeper-service.trusty android.hardware.keymaster@4.0-service.rc android.hardware.security.keymint-service.rc; do
+if [ $i = "android.hardware.gatekeeper-service.trusty" ]; then
+ cp $i p9pf/vendor/bin/hw
+elif [ $i = "android.hardware.keymaster@4.0-service.rc" ] || [ $i = "android.hardware.security.keymint-service.rc" ]; then
+ cp $i p9pf/vendor/init
+else
+ echo "Error copying files!"
+ exit 255
+fi
+done
+echo "Files copied!"
