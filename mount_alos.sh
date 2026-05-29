c191a13ad03cc3e7c8770445a0ece341e222c801
BasGame1 <abadbasantaalex@gmail.com>
2026-05-29 12:51:48 +0200

    mount alos: change '-' for '_' in the name and update as a function

--- mount_alos.sh
+++ mount_alos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mount_alos() {
 if [ ! -d system_mount ]; then
  mkdir system_mount
 fi
@@ -6,3 +7,8 @@ sudo mount -o loop alos.img system_mount
 read -p "Make all your modifications and then press enter"
 sudo umount alos.img
 rm -rf system_mount
+}
+# If the script is not sourced from the main script still can be executed as an individual file
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    mount_alos
+fi
