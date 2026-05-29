6def9247b0b186b284a71ab329fbd5c2e0882015
BasGame1 <abadbasantaalex@gmail.com>
2026-05-29 12:54:29 +0200

    create vendor: Update as a function

--- create_vendor.sh
+++ create_vendor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+create_vendor() {
 VENDOR_FILES=p9pf/vendor
 if [ -f p9pf/vendor.img ]; then
  if [ ! -d backup ]; then
@@ -7,3 +8,8 @@ if [ -f p9pf/vendor.img ]; then
  mv p9pf/vendor.img backup/vendor.img.old
 fi
 mke2fs -t ext2 -d ./p9pf/vendor p9pf/vendor.img 1300M
+}
+# If the script is not sourced from the main script still can be executed as an individual file
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    create_vendor
+fi
