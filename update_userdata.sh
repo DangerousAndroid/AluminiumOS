67563d56cefb48deff0b48a123e076ce595630c5
BasGame1 <abadbasantaalex@gmail.com>
2026-05-29 13:18:06 +0200

    update fstab, update metadata, update super disk, update userdata: Add as functions

--- update_userdata.sh
+++ update_userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+update_userdata() {
 sudo losetup -Pf super_disk.img
 LOOP_DEV=$(losetup -a | grep "super_disk.img" | awk -F: '{print $1}' | head -n 1)
 sudo dd if=userdata.img of="${LOOP_DEV}p7" bs=4M status=progress
@@ -6,3 +7,8 @@ sync
 sudo losetup -d "$LOOP_DEV"
 rm -f userdata.img
 echo "Userdata updated!"
+}
+# If the script is not sourced from the main script still can be executed as an individual file
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    update_userdata
+fi
