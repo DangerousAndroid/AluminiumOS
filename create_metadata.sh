ca3be3772f80dade901c486784ce2ccb6053df11
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-05-29 12:54:43 +0200

    create metadata: Update as a function

--- create_metadata.sh
+++ create_metadata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+create_metadata() {
 dd if=/dev/zero of=metadata.img bs=1M count=64
 mkfs.ext4 metadata.img
 mkdir -p mnt_metadata
@@ -14,3 +15,8 @@ LOOP_DEV=$(losetup -a | grep "super_disk.img" | awk -F: '{print $1}' | head -n 1
 sudo dd if=metadata.img of="${LOOP_DEV}p6" bs=4M status=progress
 sudo losetup -d "$LOOP_DEV"
 echo "Metadata Flashed to disk!"
+}
+# If the script is not sourced from the main script still can be executed as an individual file
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    create_metadata
+fi
