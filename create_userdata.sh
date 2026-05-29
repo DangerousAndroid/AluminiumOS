398f8166148e4844b580d5651dddb4b1df2b79fc
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-05-29 12:55:54 +0200

    create userdata: Update as a function

--- create_userdata.sh
+++ create_userdata.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+create_userdata() {
 truncate -s +4G super_disk.img
 sgdisk -e super_disk.img
 sgdisk -n 7:0:0 -c 7:"userdata" super_disk.img
 dd if=/dev/zero of=userdata.img bs=1M count=4096
 mkfs.ext4 -F userdata.img
+}
+# If the script is not sourced from the main script still can be executed as an individual file
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    create_userdata
+fi
