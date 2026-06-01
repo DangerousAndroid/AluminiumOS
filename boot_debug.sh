7370dd5f320dd1a3c45af116147f49daed3795b4
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-06-01 15:56:45 +0200

    boot debug: Update with some modifications to try boot with cuttlefish

--- boot_debug.sh
+++ boot_debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 boot_debug() {
 DISK="super_disk.img"
-KERNEL="./comet/boot/kernel"
-RAMDISK="./comet/vendor_boot/alos.cpio"
+KERNEL="./cuttlefish/boot/kernel"
+RAMDISK="./cuttlefish/vendor_boot/alos.cpio"
 DTB="./dtb/alos.dtb"
 CORES="12"
 MEM="16G"
@@ -14,20 +14,17 @@ qemu-system-aarch64 \
   -m $MEM \
   -accel tcg,thread=multi \
   -kernel "$KERNEL" \
-  -device loader,file="$RAMDISK",addr=0x44000000,force-raw=on \
+  -initrd "$RAMDISK" \
   -dtb "$DTB" \
-  -drive file=./RPMB_DATA,if=none,id=rpmb_drive,format=raw \
-  -device virtio-blk-pci,drive=rpmb_drive,id=rpmb-disk \
   -device qemu-xhci,id=xhci,addr=05.0 \
   -drive file="$DISK",if=none,id=super_drive,format=raw \
-  -device usb-storage,bus=xhci.0,drive=super_drive,id=super-disk \
-  -append "earlycon=pl011,0x09000000 console=ttyAMA0 root=/dev/super rw init=/init androidboot.hardware=zumapro androidboot.boot_devices=4010000000.pcie androidboot.boot_devices=pci0000:00/0000:00:05.0 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super loglevel=8 init_debug_loglevel=8 androidboot.init_rc_debug=1 androidboot.logd.kernel=true androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none" \
+  -device virtio-blk-pci,drive=super_drive,id=super-disk,addr=04.0 \
+  -append "earlycon=pl011,0x09000000 console=ttyAMA0 rw init=/init androidboot.hardware=zumapro androidboot.boot_devices=a003e00.virtio,a003000.virtio,a000000.virtio,virtio0,virtio1,virtio2,virtio3 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super loglevel=8 init_debug_loglevel=8 androidboot.init_rc_debug=1 androidboot.logd.kernel=true androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none androidboot.first_stage_console=1 androidboot.init_fatal_reboot_target=console androidboot.veritymode=eio" \
   -device virtio-tablet-pci \
-   -device virtio-gpu-gl-pci \
+  -device virtio-gpu-gl-pci \
   -display gtk,gl=on \
   -device virtio-keyboard-pci \
   -serial mon:stdio \
-  -serial file:./logs/tee.log \
   -device virtio-serial-pci,id=virtio-serial0 \
   -device virtserialport,name=com.android.emulator.secure_env,id=vc_secure_env \
   -monitor none
