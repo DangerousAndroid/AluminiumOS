a7d89e225af333367e5e1ea3cec2251417c5f4eb
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-06-01 15:55:44 +0200

    make scripts: Update

--- make_product.sh
+++ make_product.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 make_product() {
+. unpack_webview.sh || exit 255
 if [ -f cuttlefish/product.img ]; then
  if [ ! -d backup ]; then
   mkdir backup
  fi
  mv cuttlefish/product.img backup/product.img.old
 fi
-if [ ! -f cuttlefish/product/app/webview/webview.apk ]; then
- unzip -o cuttlefish/product/app/webview/webview.apk.zip -d cuttlefish/product/app/webview
-fi
-mke2fs -t ext2 -d ./cuttlefish/product cuttlefish/product.img 1300M
+unpack_webview
+mke2fs -t ext2 -d ./cuttlefish/product cuttlefish/product.img 360M
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
--- make_system_ext.sh
+++ make_system_ext.sh
@@ -6,7 +6,7 @@ if [ -f cuttlefish/system_ext.img ]; then
  fi
  mv cuttlefish/system_ext.img backup/vendor.img.old
 fi
-mke2fs -t ext2 -d ./cuttlefish/system_ext cuttlefish/system_ext.img 1300M
+mke2fs -t ext2 -d ./cuttlefish/system_ext cuttlefish/system_ext.img 600M
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
