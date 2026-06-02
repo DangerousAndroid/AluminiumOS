3e1b44934449554c7c49d2975955b5e99cf3c064
BasGame1 <abadbasantaalex@gmail.com>
2026-06-02 13:33:23 +0200

    get info: Add helper script to get info about the package manager

---
+++ get_info.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+get_info() {
+X86=0
+ARM=0
+ ARCH=$(uname -m)
+ case $ARCH in
+ "x86_64")
+ 	X86=1
+  ;;
+  "arm64")
+  	ARM=0
+  ;;
+  *)
+   	echo "$ARCH not supported, only arm64 and x86"
+  ;;
+ esac
+ for i in apt pacman yum dnf zypper snap flatpak; do
+  HAS_MANAGER=$(which $i 2>/dev/null)
+   if [ ! -z $HAS_MANAGER ]; then
+    MANAGER=$i
+    break
+   fi
+ done
+# case $MANAGER in 
+# apt)
+# 	APT=1
+# ;;
+# pacman)
+# 	PACMAN=1
+# ;;
+# yum)
+# 	YUM=1
+# ;;
+# dnf)
+# 	DNF=1
+# ;;
+# zypper)
+# 	ZYPPER=1
+# ;;
+# snap)
+# 	SNAP=1
+# ;;
+# flatpak)
+# 	FLATPAK=1
+# ;;
+# esac
+DISTRO=$(source /etc/os-release && echo "$NAME")
+}
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+   get_info
+fi
