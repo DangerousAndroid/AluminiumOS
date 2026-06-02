42230ad349f5f050ab494d861732adac8a5b667f
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-06-02 13:31:49 +0200

    boot debug: Update

--- boot_debug.sh
+++ boot_debug.sh
@@ -2,7 +2,7 @@
 boot_debug() {
 DISK="super_disk.img"
 KERNEL="./cuttlefish/boot/kernel"
-RAMDISK="./cuttlefish/vendor_boot/alos.cpio"
+RAMDISK="./comet/vendor_boot/debug.cpio"
 DTB="./dtb/alos.dtb"
 CORES="12"
 MEM="16G"
@@ -19,7 +19,7 @@ qemu-system-aarch64 \
   -device qemu-xhci,id=xhci,addr=05.0 \
   -drive file="$DISK",if=none,id=super_drive,format=raw \
   -device virtio-blk-pci,drive=super_drive,id=super-disk,addr=04.0 \
-  -append "earlycon=pl011,0x09000000 console=ttyAMA0 rw init=/init androidboot.hardware=zumapro androidboot.boot_devices=a003e00.virtio,a003000.virtio,a000000.virtio,virtio0,virtio1,virtio2,virtio3 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super loglevel=8 init_debug_loglevel=8 androidboot.init_rc_debug=1 androidboot.logd.kernel=true androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none androidboot.first_stage_console=1 androidboot.init_fatal_reboot_target=console androidboot.veritymode=eio" \
+  -append "earlycon=pl011,0x09000000 console=ttyAMA0 rw init=/init androidboot.boot_devices=a003e00.virtio,a003000.virtio,a000000.virtio,virtio0,virtio1,virtio2,virtio3 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super loglevel=8 init_debug_loglevel=8 androidboot.init_rc_debug=1 androidboot.logd.kernel=true androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none androidboot.first_stage_console=1 androidboot.init_fatal_reboot_target=console androidboot.veritymode=eio" \
   -device virtio-tablet-pci \
   -device virtio-gpu-gl-pci \
   -display gtk,gl=on \
