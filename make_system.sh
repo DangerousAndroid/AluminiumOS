1c0f3aad3f976a471e1f5672c13ed7e363999b96
BasGame1 <abadbasantaalex@gmail.com>
2026-06-03 18:03:31 +0200

    make system: Add as helper script unpack alos files to call it from here before making the image

--- make_system.sh
+++ make_system.sh
@@ -6,9 +6,11 @@ if [ -f alos.img ]; then
  fi
  mv alos.img backup/alos.img.old
 fi
+unpack_alos_files
 mke2fs -t ext2 -d ./alos alos.img 6G
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    . unpack_alos_files.sh || exit 255
     make_system
 fi
