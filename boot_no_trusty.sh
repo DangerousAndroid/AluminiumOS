19a7c295560dbb7b4dbf6bb240e87e8656c2e91f
BasGame1 <abadbasantaalex@gmail.com>
2026-06-05 13:19:38 +0200

    boot no trusty: Update with new args to boot the ctf vendor

--- boot_no_trusty.sh
+++ boot_no_trusty.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-boot_without_trusty() {
+boot_no_trusty() {
 DISK="super_disk.img"
-KERNEL="./comet/boot/kernel"
+KERNEL="./cuttlefish/boot/kernel"
 RAMDISK="./comet/vendor_boot/alos.cpio"
 DTB="./dtb/alos.dtb"
 CORES="12"
 MEM="16G"
 echo "Launching AluminiumOS (Build CP1A.260305.018) without Trusty TEE..."
-# This version uses the general qemu bin from the package manager
 qemu-system-aarch64 \
   -M virt,gic-version=3 \
   -cpu max,sve=off \
@@ -15,24 +14,22 @@ qemu-system-aarch64 \
   -m $MEM \
   -accel tcg,thread=multi \
   -kernel "$KERNEL" \
-  -device loader,file="$RAMDISK",addr=0x44000000,force-raw=on \
+  -initrd "$RAMDISK" \
   -dtb "$DTB" \
-  -drive file=./RPMB_DATA,if=none,id=rpmb_drive,format=raw \
-  -device virtio-blk-pci,drive=rpmb_drive,id=rpmb-disk \
   -device qemu-xhci,id=xhci,addr=05.0 \
   -drive file="$DISK",if=none,id=super_drive,format=raw \
-  -device usb-storage,bus=xhci.0,drive=super_drive,id=super-disk \
-  -append "earlycon=pl011,0x09000000 console=ttyAMA0 root=/dev/super rw init=/init androidboot.hardware=zumapro androidboot.boot_devices=4010000000.pcie androidboot.boot_devices=pci0000:00/0000:00:05.0 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super loglevel=3	 androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none" \
-  -device ramfb \
+  -device virtio-blk-pci,drive=super_drive,id=super-disk,addr=04.0 \
+  -append "earlycon=pl011,0x09000000 console=ttyAMA0 rw init=/init androidboot.boot_devices=4010000000.pcie,pci0000:00/0000:00:04.0,virtio3 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super androidboot.hardware=zuma loglevel=3 androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none androidboot.first_stage_console=1 printk.devkmsg=on androidboot.veritymode=enforcing loop.max_loop=256 ro.apex.updatable=true androidboot.vendor.apex.com.android.hardware.graphics.composer=com.android.hardware.graphics.composer.drm_hwcomposer.apex stack_depot_disable=on cgroup_disable=pressure kasan.stacktrace=off androidboot.vendor.apex.com.android.hardware.keymint=com.android.hardware.keymint.rust_nonsecure.apex androidboot.vendor.apex.com.android.hardware.gatekeeper=com.android.hardware.gatekeeper.nonsecure.apex androidboot.vendor.apex.com.google.emulated.camera.provider.hal=com.google.emulated.camera.provider.hal.apex" \
+  -device virtio-tablet-pci \
+  -device virtio-gpu-gl-pci \
   -display gtk,gl=on \
   -device virtio-keyboard-pci \
   -serial mon:stdio \
-  -serial file:./logs/tee.log \
   -device virtio-serial-pci,id=virtio-serial0 \
   -device virtserialport,name=com.android.emulator.secure_env,id=vc_secure_env \
   -monitor none
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    boot_without_trusty
+    boot_no_trusty
 fi
