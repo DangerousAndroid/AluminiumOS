273b8e77bf093788066a14386d32cf0333eccdbc
BasGame1 <abadbasantaalex@gmail.com>
2026-06-05 13:19:06 +0200

    unpack alos files: Fix a few things, like syntax errors

--- unpack_alos_files.sh
+++ unpack_alos_files.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 unpack_alos_files() {
-APEX_DIR=alos/system/product/apex
-SATIN_DIR=alos/system/product/priv-app/satin
-cd $APEX_DIR
+APEX_DIR=$ROOT_DIR/alos-gsi/system/product/apex
+SATIN_DIR=$ROOT_DIR/alos-gsi/system/product/priv-app/Satin
+cd $SATIN_DIR
 if [ -f satin.tar.xz.ab ] && [ -f satin.tar.xz.aa ]; then
   cat "satin.tar.xz.aa" "satin.tar.xz.ab" | tar -xJf -
 else
  error "Satin splits missing"
 fi
-cd $ROOT_DIR && cd $APEX_DIR
-if [ -f scom.google.android.gmssystem.prodvic.tar.xzab ] && [ -f com.google.android.gmssystem.prodvic.tar.xzaa ]; then
+cd $APEX_DIR
+if [ -f com.google.android.gmssystem.prodvic.tar.xzab ] && [ -f com.google.android.gmssystem.prodvic.tar.xzaa ]; then
   cat "com.google.android.gmssystem.prodvic.tar.xzaa" "com.google.android.gmssystem.prodvic.tar.xzab" | tar -xJf -
 else
  error "Apex splits missing"
@@ -17,7 +17,7 @@ fi
 cd $ROOT_DIR
 }
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-   $ROOT_DIR=$(pwd)
+   ROOT_DIR=$(pwd)
    . colors.sh || exit 255
    unpack_alos_files
 fi
