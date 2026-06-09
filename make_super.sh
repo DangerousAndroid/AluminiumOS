53fb5b3127629e7706b3d939ffe8e9121a805e80
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-06-09 14:20:04 +0200

    make super test: Rename to make super due to being now the main one

--- make_super.sh
+++ make_super.sh
@@ -6,6 +6,7 @@ if [ -f super_alos.img ]; then
  fi
  mv super_alos.img backup/super_alos.img.old
 fi
+# System ext and product are inncesary due to being inside system
 lpmake \
     --metadata-size 65536 \
     --super-name super \
@@ -15,22 +16,16 @@ lpmake \
     --partition system:readonly:$(stat -c%s alos.img):google_dynamic_partitions \
     --image system=./alos.img \
     --partition vendor:readonly:$(stat -c%s comet/vendor.img):google_dynamic_partitions \
-    --image vendor=comet/vendor.img \
-    --partition product:readonly:$(stat -c%s cuttlefish/product.img):google_dynamic_partitions \
-    --image product=cuttlefish/product.img \
-    --partition system_ext:readonly:$(stat -c%s cuttlefish/system_ext.img):google_dynamic_partitions \
-    --image system_ext=cuttlefish/system_ext.img \
-    --partition odm:readonly:0:google_dynamic_partitions \
+    --image vendor=cuttlefish/vendor.img \
+    --partition product:readonly:0:google_dynamic_partitions \
+    --partition system_ext:readonly:0:google_dynamic_partitions \
+    --partition odm_dlkm:readonly:$(stat -c%s cuttlefish/odm_dlkm.img):google_dynamic_partitions \
+    --image odm_dlkm=cuttlefish/odm_dlkm.img \
     --partition system_dlkm:readonly:$(stat -c%s cuttlefish/system_dlkm.img):google_dynamic_partitions \
     --image system_dlkm=cuttlefish/system_dlkm.img \
     --partition vendor_dlkm:readonly:$(stat -c%s cuttlefish/vendor_dlkm.img):google_dynamic_partitions \
     --image vendor_dlkm=cuttlefish/vendor_dlkm.img \
     --output super_alos.img
-if [ $? != 0 ]; then
- for i in cuttlefish/vendor_dlkm.img comet/vendor.img cuttlefish/system_dlkm.img cuttlefish/product.img cuttlefish/system_ext.img; do
-  rm -rf $i
- done
-fi
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
