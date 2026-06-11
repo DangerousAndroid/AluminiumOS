5cdebbe4218299450ad4e32727dfdcf82e09b8a6
BasGame1 <abadbasantaalex@gmail.com>
2026-06-12 00:21:18 +0200

    update ctf fstab: Init helper script that changes cutf fstabs and not comet

---
+++ update_ctf_fstab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+update_ctf_fstab() {
+cd cuttlefish/vendor_boot
+# Cuttlefish fstabs (name)
+magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.cutf_cvm fstab.cutf_cvm"
+# Cuttelefish fstabs (platform)
+magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.vsoc_arm64 fstab.vsoc_arm64"
+# Add default.prop
+#magiskboot cpio alos.cpio "add 0750 prop.default prop.default"
+# Add comet default.prop
+magiskboot cpio alos.cpio "add 0750 prop.default prop.comet.default"
+cd $ROOT_DIR
+}
+# If the script is not sourced from the main script still can be executed as an individual file
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    ROOT_DIR=$(pwd)
+    update_ctf_fstab
+fi
