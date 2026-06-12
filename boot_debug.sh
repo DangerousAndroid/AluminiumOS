cfbd5a186289b131f33963fe1827a861663e62f9
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-06-12 10:58:34 +0200

    boot debug: Revert to comet ramdisk due to inability to find fstab on the cutf one

--- boot_debug.sh
+++ boot_debug.sh
@@ -2,7 +2,7 @@
 boot_debug() {
 DISK="super_disk.img"
 KERNEL="./cuttlefish/boot/kernel"
-RAMDISK="./cuttlefish/vendor_boot/alos.cpio"
+RAMDISK="./comet/vendor_boot/alos.cpio"
 DTB="./dtb/alos.dtb"
 CORES="12"
 MEM="16G"
@@ -20,7 +20,7 @@ qemu-system-aarch64 \
   -drive file="$DISK",if=none,id=super_drive,format=raw \
   -device virtio-blk-pci,drive=super_drive,id=super-disk,addr=04.0 \
   -device virtio-gpu-gl-pci,id=gpu0,addr=06.0,blob=true,max_outputs=1 \
-  -append "earlycon=pl011,0x09000000 console=ttyAMA0 rw init=/init androidboot.boot_devices=4010000000.pcie,pci0000:00/0000:00:04.0,virtio3 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super loglevel=8 init_debug_loglevel=8 androidboot.init_rc_debug=1 androidboot.logd.kernel=true androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none androidboot.first_stage_console=1 printk.devkmsg=on androidboot.veritymode=enforcing loop.max_loop=256 ro.apex.updatable=true androidboot.vendor.apex.com.android.hardware.graphics.composer=com.android.hardware.graphics.composer.drm_hwcomposer.apex stack_depot_disable=on cgroup_disable=pressure kasan.stacktrace=off androidboot.vendor.apex.com.android.hardware.keymint=com.android.hardware.keymint.rust_nonsecure.apex androidboot.vendor.apex.com.android.hardware.gatekeeper=com.android.hardware.gatekeeper.nonsecure.apex androidboot.vendor.apex.com.google.emulated.camera.provider.hal=com.google.emulated.camera.provider.hal.apex androidboot.slot_suffix=_a androidboot.hardware.egl=emulation androidboot.hardware.gralloc=minigbm androidboot.hardware.hwcomposer=ranchu androidboot.hardware.vulkan=0 androidboot.config.hwcomposer=ranchu androidboot.hardware.hwcomposer.mode=client androidboot.hardware=zuma androidboot.hardware.hwcomposer.display_finder_mode=default" \
+  -append "androidboot.fstab_from_fdt=0 earlycon=pl011,0x09000000 console=ttyAMA0 rw init=/init androidboot.boot_devices=4010000000.pcie,pci0000:00/0000:00:04.0,virtio3 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super loglevel=8 init_debug_loglevel=8 androidboot.init_rc_debug=1 androidboot.logd.kernel=true androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none androidboot.first_stage_console=1 printk.devkmsg=on androidboot.veritymode=enforcing loop.max_loop=256 ro.apex.updatable=true androidboot.vendor.apex.com.android.hardware.graphics.composer=com.android.hardware.graphics.composer.drm_hwcomposer.apex stack_depot_disable=on cgroup_disable=pressure kasan.stacktrace=off androidboot.vendor.apex.com.android.hardware.keymint=com.android.hardware.keymint.rust_nonsecure.apex androidboot.vendor.apex.com.android.hardware.gatekeeper=com.android.hardware.gatekeeper.nonsecure.apex androidboot.vendor.apex.com.google.emulated.camera.provider.hal=com.google.emulated.camera.provider.hal.apex androidboot.slot_suffix=_a androidboot.hardware.egl=emulation androidboot.hardware.gralloc=minigbm androidboot.hardware.hwcomposer=ranchu androidboot.hardware.vulkan=0 androidboot.config.hwcomposer=ranchu androidboot.hardware.hwcomposer.mode=client androidboot.hardware=zuma androidboot.hardware.hwcomposer.display_finder_mode=default" \
   -device virtio-tablet-pci \
   -display sdl,gl=on,show-cursor=on \
   -device virtio-keyboard-pci \
