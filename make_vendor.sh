b498d135fc2b847c469dc8ccad20a5ce5c814ffa
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-06-12 15:37:37 +0200

    make vendor: Use logger to show info, errors and success

--- make_vendor.sh
+++ make_vendor.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 make_vendor() {
+log_info "Searching for backup"
 if [ -f cuttlefish/vendor.img ]; then
  if [ ! -d backup ]; then
   mkdir backup
@@ -7,8 +8,11 @@ if [ -f cuttlefish/vendor.img ]; then
  mv cuttlefish/vendor.img backup/vendor.img.old
 fi
 mke2fs -t ext4 -O ext_attr,has_journal,dir_index,sparse_super -d ./cuttlefish/vendor cuttlefish/vendor.img 1300M
+log_success "Vendor image built"
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    . colors.sh || exit 255
+    . logger.sh || exit 255
     make_vendor
 fi
