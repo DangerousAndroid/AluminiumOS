6161396c79cf9900c3bd338354b2a3889cd248a5
BasGame1 <abadbasantaalex@gmail.com>
2026-06-12 15:28:13 +0200

    make super: Update with colors

--- make_super.sh
+++ make_super.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 make_super() {
+log "Searching for backup"
 if [ -f super_alos.img ]; then
  if [ ! -d backup ]; then
    mkdir backup
  fi
  mv super_alos.img backup/super_alos.img.old
 fi
+log_info "Building super image"
 # System ext and product are inncesary due to being inside system
 lpmake \
     --metadata-size 65536 \
@@ -26,8 +28,10 @@ lpmake \
     --partition vendor_dlkm:readonly:$(stat -c%s cuttlefish/vendor_dlkm.img):google_dynamic_partitions \
     --image vendor_dlkm=cuttlefish/vendor_dlkm.img \
     --output super_alos.img
+log_sucess "Super image built"
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    . colors.sh || exit 255
     make_super
 fi
