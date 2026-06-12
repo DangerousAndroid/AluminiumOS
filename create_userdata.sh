5ce014f1d7ca69dc74c2df66acaa847022f7c576
BasGame1 <abadbasantaalex@gmail.com>
2026-06-12 12:56:50 +0200

    create userdata: Call update userdata

--- create_userdata.sh
+++ create_userdata.sh
@@ -5,8 +5,10 @@ sgdisk -e super_disk.img
 sgdisk -n 7:0:0 -c 7:"userdata" super_disk.img
 dd if=/dev/zero of=userdata.img bs=1M count=4096
 mkfs.ext4 -F userdata.img
+update_userdata
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    . update_userdata.sh || exit 255
     create_userdata
 fi
