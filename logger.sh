59de56f10107b7ac730a986d97f22ec4239d932d
BasGame1 <abadbasantaalex@gmail.com>
2026-06-12 15:27:18 +0200

    logger: Make the default option logger on and add log_success

--- logger.sh
+++ logger.sh
@@ -1,6 +1,18 @@
-log() {
+log_info() {
 echo ""
+echo ""
+info "$1"
+echo "[INFO]: $1" >> logs/script.log
 }
 log_error() {
 echo ""
+echo ""
+error "$1"
+echo "[ERROR]: $1" >> logs/script.log
+}
+log_success() {
+echo ""
+echo ""
+success "$1"
+echo "[SUCCESS]: $1" >> logs/script.log
 }
