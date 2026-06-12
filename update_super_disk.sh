e3d90ca90565d6cddfa228c94f0262d85e1fa94e
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-06-12 15:38:05 +0200

    update super disk: Update with log features

--- update_super_disk.sh
+++ update_super_disk.sh
@@ -3,16 +3,18 @@ update_super_disk() {
 sudo losetup -Pf super_disk.img
 LOOP_DEV=$(losetup -a | grep "super_disk.img" | awk -F: '{print $1}' | head -n 1)
 if [ -z "$LOOP_DEV" ]; then
-    echo "Error: Could not map super_disk.img to a loop device."
+    log_echo "Error: Could not map super_disk.img to a loop device."
     exit 1
 fi
 sudo dd if=super_alos.img of="${LOOP_DEV}p5" bs=4M status=progress
 sync
 sudo losetup -d "$LOOP_DEV"
 rm -rf super_alos.img
-echo "Super updated in disk!"
+log_success "Super updated in disk!"
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    . colors.sh || exit 255
+    . logger.sh || exit 255
     update_super_disk
 fi
