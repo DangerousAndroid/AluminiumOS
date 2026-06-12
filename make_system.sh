13d0ba7d38e13aaeb38c1a44f9c5538520a93fa4
BasGame1 <abadbasantaalex@gmail.com>
2026-06-12 15:36:52 +0200

    make system: Update with logger functions

--- make_system.sh
+++ make_system.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 make_system() {
+log_info "Searching for backups"
 if [ -f alos.img ]; then
  if [ ! -d backup ]; then
   mkdir backup
@@ -8,11 +9,13 @@ if [ -f alos.img ]; then
 fi
 unpack_alos_files
 mke2fs -t ext4 -O ext_attr,has_journal,dir_index,sparse_super -d ./alos-gsi alos.img 6G
+log_success "System image built"
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     ROOT_DIR=$(pwd)
     . colors.sh || exit 255
+    . logger.sh || exit 255
     . unpack_alos_files.sh || exit 255
     make_system
 fi
