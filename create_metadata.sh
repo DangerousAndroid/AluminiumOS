6248b0d10681679c9327d87c4f69c2946d9680a3
BasGame1 <abadbasantaalex@gmail.com>
2026-06-12 12:54:22 +0200

    create metadata: Change from creating a pre populated image to install it in super a black one

--- create_metadata.sh
+++ create_metadata.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 create_metadata() {
-dd if=/dev/zero of=metadata.img bs=1M count=64
-mkfs.ext4 metadata.img
-mkdir -p mnt_metadata
-sudo mount -o loop metadata.img mnt_metadata
-sudo mkdir -p mnt_metadata/aconfig/maps
-sudo mkdir -p mnt_metadata/aconfig/boot
-sudo chmod -R 777 mnt_metadata/aconfig
-sudo umount mnt_metadata
-rmdir mnt_metadata
-echo "Metadata created!"
-sudo losetup -Pf super_disk.img
-LOOP_DEV=$(losetup -a | grep "super_disk.img" | awk -F: '{print $1}' | head -n 1)
-sudo dd if=metadata.img of="${LOOP_DEV}p6" bs=4M status=progress
-sudo losetup -d "$LOOP_DEV"
-echo "Metadata Flashed to disk!"
+truncate -s +1G super_disk.img
+sgdisk -e super_disk.img
+sgdisk -n 6:0:0 -c 6:"metadata" super_disk.img
+dd if=/dev/zero of=metadata.img bs=1M count=1024
+mkfs.ext4 -F metadata.img
+update_metadata
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    . update_metadata.sh || exit 255
     create_metadata
 fi
