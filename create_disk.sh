4dae30bd5432f12e900693d0842d8d675c53268b
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-06-24 21:41:56 +0200

    create disk: Add log features

--- create_disk.sh
+++ create_disk.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 create_disk() {
-SUPER_IMAGE=super_alos.img
 read -p "Start? It will ocuppy in the process +20gb" -n1
 dd if=/dev/zero of=super_disk.img bs=1M count=13000
 sgdisk -n 1:2048:+64M   -c 1:"boot"          super_disk.img
@@ -13,9 +12,12 @@ LOOP_DEV=$(losetup -a | grep "super_disk.img" | awk -F: '{print $1}' | head -n 1
 echo "Disk mapped to $LOOP_DEV."
 sudo dd if=$SUPER_IMAGE of="${LOOP_DEV}p5" bs=4M status=progress
 sudo losetup -d "$LOOP_DEV"
-echo "Done!"
+log_success "Done!"
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    . vars.sh || exit 255
+    . colors.sh || exit 255
+    . logger.sh || exit 255
     create_disk
 fi
