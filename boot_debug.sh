f71b5d29a1204c802b9a2f5350965f4ea42cbae9
BasGame1 <abadbasantaalex@gmail.com>
2026-06-24 21:41:13 +0200

    boot debug & boot no trusty: Move vars to vars file

--- boot_debug.sh
+++ boot_debug.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 boot_debug() {
-DISK="super_disk.img"
-KERNEL="./cuttlefish/boot/kernel"
-RAMDISK="./comet/vendor_boot/alos.cpio"
-DTB="./dtb/alos.dtb"
-CORES="12"
-MEM="16G"
-
-echo "Launching AluminiumOS (Build CP1A.260305.018) without Trusty TEE..."
+log "Launching AluminiumOS (Build CP1A.260305.018) without Trusty TEE..."
 qemu-system-aarch64 \
   -M virt,gic-version=3 \
   -cpu max,sve=off \
@@ -21,7 +14,7 @@ qemu-system-aarch64 \
   -drive file="$DISK",if=none,id=super_drive,format=raw \
   -device virtio-blk-pci,drive=super_drive,id=super-disk,addr=04.0 \
   -device virtio-gpu-gl-pci,id=gpu0,addr=06.0,blob=true,max_outputs=1 \
-  -append "persist.sys.angle.backend=2 persist.graphics.egl=emulation ro.hardware.egl=emulation ro.board.platform=android androidboot.fstab_from_fdt=0 earlycon=pl011,0x09000000 console=ttyAMA0 rw init=/init androidboot.boot_devices=4010000000.pcie,pci0000:00/0000:00:04.0,virtio3 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super loglevel=8 init_debug_loglevel=8 androidboot.init_rc_debug=1 androidboot.logd.kernel=true androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none androidboot.first_stage_console=1 printk.devkmsg=on androidboot.veritymode=enforcing loop.max_loop=256 ro.apex.updatable=true androidboot.vendor.apex.com.android.hardware.graphics.composer=com.android.hardware.graphics.composer.drm_hwcomposer.apex stack_depot_disable=on cgroup_disable=pressure kasan.stacktrace=off androidboot.vendor.apex.com.android.hardware.keymint=com.android.hardware.keymint.rust_nonsecure.apex androidboot.vendor.apex.com.android.hardware.gatekeeper=com.android.hardware.gatekeeper.nonsecure.apex androidboot.vendor.apex.com.google.emulated.camera.provider.hal=com.google.emulated.camera.provider.hal.apex androidboot.slot_suffix=_a androidboot.hardware.egl=mesa androidboot.hardware.gralloc=gbm androidboot.hardware.hwcomposer=drm_hwcomposer androidboot.hardware.vulkan=0 androidboot.config.hwcomposer=drm_hwcomposer androidboot.hardware=zuma" \
+  -append "$CMDLINE_DEBUG" \
   -device virtio-tablet-pci \
   -display sdl,gl=on,show-cursor=on \
   -device virtio-keyboard-pci \
@@ -33,5 +26,8 @@ qemu-system-aarch64 \
 
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    . vars.sh || exit 255
+    . colors.sh || exit 255
+    . logger.sh || exit 255
     boot_debug
 fi
--- boot_no_trusty.sh
+++ boot_no_trusty.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 boot_no_trusty() {
-DISK="super_disk.img"
-KERNEL="./cuttlefish/boot/kernel"
-RAMDISK="./comet/vendor_boot/alos.cpio"
-DTB="./dtb/alos.dtb"
-CORES="12"
-MEM="16G"
-echo "Launching AluminiumOS (Build CP1A.260305.018) without Trusty TEE..."
+log "Launching AluminiumOS (Build CP1A.260305.018) without Trusty TEE..."
 qemu-system-aarch64 \
   -M virt,gic-version=3 \
   -cpu max,sve=off \
@@ -19,7 +13,7 @@ qemu-system-aarch64 \
   -device qemu-xhci,id=xhci,addr=05.0 \
   -drive file="$DISK",if=none,id=super_drive,format=raw \
   -device virtio-blk-pci,drive=super_drive,id=super-disk,addr=04.0 \
-  -append "persist.sys.angle.backend=2 persist.graphics.egl=emulation ro.hardware.egl=emulation ro.board.platform=android earlycon=pl011,0x09000000 console=ttyAMA0 rw init=/init androidboot.boot_devices=4010000000.pcie,pci0000:00/0000:00:04.0,virtio3 androidboot.selinux=permissive androidboot.lcd_density=160 androidboot.super_partition=super androidboot.hardware=zuma loglevel=0 androidboot.force_normal_boot=1 androidboot.vbmeta.device_state=unlocked androidboot.verifiedbootstate=orange kvm-arm.mode=none androidboot.first_stage_console=1 printk.devkmsg=on androidboot.veritymode=enforcing loop.max_loop=256 ro.apex.updatable=true androidboot.vendor.apex.com.android.hardware.graphics.composer=com.android.hardware.graphics.composer.drm_hwcomposer.apex stack_depot_disable=on cgroup_disable=pressure kasan.stacktrace=off androidboot.vendor.apex.com.android.hardware.keymint=com.android.hardware.keymint.rust_nonsecure.apex androidboot.vendor.apex.com.android.hardware.gatekeeper=com.android.hardware.gatekeeper.nonsecure.apex androidboot.vendor.apex.com.google.emulated.camera.provider.hal=com.google.emulated.camera.provider.hal.apex" \
+  -append $CMDLINE \
   -device virtio-tablet-pci \
   -device virtio-gpu-gl-pci,id=gpu0,addr=06.0,blob=true,max_outputs=1 \
   -display sdl,gl=on,show-cursor=on \
@@ -31,5 +25,8 @@ qemu-system-aarch64 \
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    . vars.sh || exit 255
+    . colors.sh || exit 255
+    . logger.sh || exit 255
     boot_no_trusty
 fi
