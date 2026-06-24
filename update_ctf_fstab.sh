acbff95d68f7d53284fa2f8e017d54f75ec3040e
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-06-24 21:38:14 +0200

    update ctf fstab & update fstab: Add vars file with magiskboot binary

--- update_ctf_fstab.sh
+++ update_ctf_fstab.sh
@@ -2,17 +2,17 @@
 update_ctf_fstab() {
 cd cuttlefish/vendor_boot
 # Cuttlefish fstabs (name)
-magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.cutf_cvm fstab.cutf_cvm"
+$MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.cutf_cvm fstab.cutf_cvm"
 # Cuttelefish fstabs (platform)
-magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.vsoc_arm64 fstab.vsoc_arm64"
+$MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.vsoc_arm64 fstab.vsoc_arm64"
 # Add default.prop
 #magiskboot cpio alos.cpio "add 0750 prop.default prop.default"
 # Add comet default.prop
-magiskboot cpio alos.cpio "add 0750 prop.default prop.comet.default"
+$MAGISKBOOT cpio alos.cpio "add 0750 prop.default prop.comet.default"
 cd $ROOT_DIR
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    ROOT_DIR=$(pwd)
+    . vars.sh || exit 255
     update_ctf_fstab
 fi
