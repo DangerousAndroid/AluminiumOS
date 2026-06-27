3e3a9ff903751c7577b4b3d426a6b81534b5b6af
BasGame1 <abadbasantaalex@gmail.com>
2026-06-27 16:53:26 +0200

    various files: Update

--- make_system.sh
+++ make_system.sh
@@ -8,7 +8,8 @@ if [ -f alos.img ]; then
  mv alos.img backup/alos.img.old
 fi
 unpack_alos_files
-mke2fs -t ext4 -O ext_attr,has_journal,dir_index,sparse_super -d ./alos-gsi alos.img 6G
+mke2fs -t ext4 -O ext_attr,has_journal,dir_index,sparse_super alos.img 6G
+e2fsdroid -e -S ./alos-gsi/system/etc/selinux/plat_file_contexts -f ./alos-gsi -a / alos.img
 log_success "System image built"
 }
 # If the script is not sourced from the main script still can be executed as an individual file
--- update_fstab.sh
+++ update_fstab.sh
@@ -4,7 +4,7 @@ cd comet/vendor_boot
 # Cuttlefish fstabs (stock ones)
 #magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.cf.ext4.cts fstab.cf.ext4.cts" && magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.cf.ext4.hctr2 fstab.cf.ext4.hctr2" && magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.cf.f2fs.cts fstab.cf.f2fs.cts" && magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.cf.f2fs.hctr2 fstab.cf.f2fs.hctr2"
 # Comet/zuma fstabs (deprecated due to using cuttlefish ones now but still usable witb comet vendor)
-$MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zuma fstab.zuma" && $MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zuma-fips fstab.zuma-fips" && magiskboot cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zumapro fstab.zumapro" && $MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zumapro-fips fstab.zumapro-fips" && $MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/recovery.fstab recovery.fstab"
+$MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zuma fstab.zuma" && $MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zuma-fips fstab.zuma-fips" && $MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zumapro fstab.zumapro" && $MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.zumapro-fips fstab.zumapro-fips" && $MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/recovery.fstab recovery.fstab"
 # Cuttelefish fstabs (modified ones)
 $MAGISKBOOT cpio alos.cpio "add 0750 first_stage_ramdisk/system/etc/fstab.vsoc_arm64 fstab.vsoc_arm64"
 log_success "Fstab updated"
