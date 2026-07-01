3c2d611a0f243ed24f2d8984bdc0334004bb61a0
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-07-01 15:16:16 +0200

    various files: Update make scripts

--- make_product.sh
+++ make_product.sh
@@ -7,7 +7,8 @@ if [ -f cuttlefish/product.img ]; then
  mv cuttlefish/product.img backup/product.img.old
 fi
 unpack_webview
-mke2fs -t ext2 -d ./cuttlefish/product cuttlefish/product.img 360M
+mke2fs -t ext4 -O ext_attr,has_journal,dir_index,sparse_super cuttlefish/product.img 360M
+e2fsdroid -e -S ./cuttlefish/product/etc/selinux/product_file_contexts -f ./cuttlefish/product -a /product cuttlefish/product.img
 }
 # If the script is not sourced from the main script still can be executed as an individual file
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
--- make_system.sh
+++ make_system.sh
@@ -9,7 +9,15 @@ if [ -f alos.img ]; then
 fi
 unpack_alos_files
 mke2fs -t ext4 -O ext_attr,has_journal,dir_index,sparse_super alos.img 6G
-e2fsdroid -e -S ./alos-gsi/system/etc/selinux/plat_file_contexts -f ./alos-gsi -a / alos.img
+cat ./alos-gsi/system/etc/selinux/plat_file_contexts \
+    ./alos-gsi/system/system_ext/etc/selinux/system_ext_file_contexts \
+    ./alos-gsi/system/product/etc/selinux/product_file_contexts > ./cuttlefish/unified_plat_file_contexts
+sed -i 's@^/system_ext/@/(system_ext|system/system_ext)/@' ./cuttlefish/unified_plat_file_contexts
+sed -i 's@^/product/@/(product|system/product)/@' ./cuttlefish/unified_plat_file_contexts
+echo "/(system_ext|system/system_ext)/bin/rpmb_dev\.desktop\.system u:object_r:rpmb_dev_system_exec:s0" >> ./cuttlefish/unified_plat_file_contexts
+echo "/(system_ext|system/system_ext)/bin/rpmb_dev\.system u:object_r:rpmb_dev_system_exec:s0" >> ./cuttlefish/unified_plat_file_contexts
+echo "/(system_ext|system/system_ext)/bin/storageproxyd\.system u:object_r:storageproxyd_system_exec:s0" >> ./cuttlefish/unified_plat_file_contexts
+e2fsdroid -e -S ./cuttlefish/unified_plat_file_contexts -f ./alos-gsi -a / alos.img
 log_success "System image built"
 }
 # If the script is not sourced from the main script still can be executed as an individual file
