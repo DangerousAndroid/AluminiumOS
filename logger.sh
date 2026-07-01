d9b55e1900aa2350bb742e44854b7601b70661f4
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-07-01 21:39:04 +0200

    logger: Update

--- logger.sh
+++ logger.sh
@@ -1,18 +1,1 @@
-log_info() {
-echo ""
-echo ""
-info "$1"
-echo "[INFO]: $1" >> "$ROOT"/logs/script.log
-}
-log_error() {
-echo ""
-echo ""
-error "$1"
-echo "[ERROR]: $1" >> "$ROOT"/logs/script.log
-}
-log_success() {
-echo ""
-echo ""
-success "$1"
-echo "[SUCCESS]: $1" >> "$ROOT"/logs/script.log
-}
+log_info() {\necho ""\necho ""\ninfo "$1"\necho "[INFO]: $1" >> logs/script.log\n}\nlog_error() {\necho ""\necho ""\nerror "$1"\necho "[ERROR]: $1" >> logs/script.log\n}\n log_green() {\necho ""\necho ""\nsuccess "$1"\necho "[SUCCESS]: $1" >> logs/script.log\n}
