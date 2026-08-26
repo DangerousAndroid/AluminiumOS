70a43abd7591defafcdc35243726307e0b5ed70d
DangerousAndroid <sindirecciondecorreo61@gmail.com>
2026-08-26 17:08:54 +0200

    fix(disk): unsparse super_alos.img using simg2img before writing to super_disk

--- update_super_disk.sh
+++ update_super_disk.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 update_super_disk() {
+echo "Unsparsing super image..."
+simg2img super_alos.img super_raw.img
 echo "Writing super image to disk (offset 97M)..."
-dd if=super_alos.img of=super_disk.img bs=1M seek=97 conv=notrunc status=progress
+dd if=super_raw.img of=super_disk.img bs=1M seek=97 conv=notrunc status=progress
 sync
-rm -rf super_alos.img
+rm -rf super_alos.img super_raw.img
 log_success "Super updated in disk!"
 }
 # If the script is not sourced from the main script still can be executed as an individual file
